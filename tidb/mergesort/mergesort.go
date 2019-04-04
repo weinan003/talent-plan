0397b54859c5ff7d908884a55c0f232191d0bf28
Weinan Wang <wewang@pivotal.io>
2019-04-04 17:43:16 +0800

    WIP: mergesort

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,6 +1,44 @@
 package main
 
+import "sync"
+
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
+
+
+func Conquer(src []int64, anchor int) {
+	lhsid := 0
+	rhsid := anchor
+
+	for lhsid < anchor && rhsid < len(src) {
+		for lhsid < anchor && src[lhsid] z
+	}
+}
+
+func Divide(src []int64,parentWG *sync.WaitGroup) {
+
+	if(len(src) >= 2) {
+		var wg sync.WaitGroup
+
+		wg.Add(2)
+		go Divide(src[:len(src)/2], &wg)
+		go Divide(src[len(src)/2:], &wg)
+		wg.Wait()
+
+		Conquer(src, len(src) / 2)
+	}
+
+	parentWG.Done()
+}
+
 func MergeSort(src []int64) {
+
+	if len(src) < 2 {
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	Divide(src, &wg)
+	wg.Done()
 }
